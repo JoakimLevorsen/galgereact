eee655af78bbe05f1a9458728b745b9471d95212
Cecilie Krog Drejer <43748670+drellert@users.noreply.github.com>
2020-03-09 13:32:38 +0100

    HighScore class with save/load methods implemented

--- src/highscore.ts
+++ src/highscore.ts
@@ -1,22 +1,50 @@
 export default class HighScore {
-    public timeInMs: number
-    public highScore: number
-    readonly twoMinInMs = 120000
+    public timeInMs: number;
+    public score: number;
+    readonly twoMinInMs = 120000;
 
-    constructor(public name: string, public time: Date, public word: string, public wrongLettersGuessed: number) {
-        this.timeInMs = time.getTime()
-        this.highScore = this.calcScore()
+    constructor(
+        public name: string,
+        public time: Date,
+        public word: string,
+        public wrongLettersGuessed: number
+    ) {
+        this.timeInMs = time.getTime();
+        this.score = this.calcScore();
     }
 
     private calcScore(): number {
-        return (this.twoMinInMs - this.timeInMs + (500 * this.word.length) - (1000 * this.wrongLettersGuessed))
-    }
-
-    private saveHighScore(highScore: HighScore) {
-
-    }
-
-    private loadHighScores() {
-
+        return (
+            this.twoMinInMs -
+            this.timeInMs +
+            500 * this.word.length -
+            500 * this.wrongLettersGuessed
+        );
     }
 }
+
+const key = "highScore";
+
+export const saveHighScore = (highScore: HighScore) => {
+    if (typeof Storage) {
+        let highScoreArray = loadHighScores();
+        highScoreArray.push(highScore);
+        if (highScoreArray.length > 1) {
+            highScoreArray.sort(function(a, b) {
+                return b.score - a.score;
+            });
+        }
+        localStorage.setItem(key, JSON.stringify(highScoreArray));
+    } else {
+        alert("Sorry, your browser does not support web storage.");
+    }
+};
+
+export const loadHighScores = (): HighScore[] => {
+    if (localStorage) {
+        return JSON.parse(localStorage.getItem(key) ?? "[]") as HighScore[];
+    } else {
+        alert("Sorry, your browser does not support web storage.");
+        return new Array<HighScore>(0);
+    }
+};
